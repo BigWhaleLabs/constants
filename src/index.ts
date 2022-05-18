caaebd6f5aabd2af7f837972594525ac311f37b4
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-05-18 08:47:21 -0700

    bump dependencies, fix release script, change names of vars

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ export const ETH_NETWORK = 'rinkeby'
 export const ETH_RPC = 'https://rinkeby.sealcred.xyz/rpc'
 export const ETH_WS = 'wss://rinkeby.sealcred.xyz/ws'
 export const BITSKI_CLIENT_ID = 'b860c339-5e6d-4453-8189-155f872b2fc8'
-export const SC_LEDGER_CONTRACT_ADDRESS =
+export const SCLEDGER_CONTRACT_ADDRESS =
   '0x11E44Ee2F9B5Ae5f9bD7d018A3dcdE7eDaEdD212'
-export const CONTRACT_ADDRESS = '0x722B0676F457aFe13e479eB2a8A4De88BA15B2c6'
+export const DOSU_INVITES_CONTRACT_ADDRESS =
+  '0x722B0676F457aFe13e479eB2a8A4De88BA15B2c6'
 export const QUERY_BLOCK_LIMIT = 500000
