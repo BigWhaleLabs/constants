7d202cc5eb45d827361a0e6fbec58c698055eea4
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-13 12:33:40 -0700

    switch to goerli

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ export const ETH_NETWORK = 'goerli'
 export const ETH_RPC = 'https://gorli.sealcred.xyz/rpc'
 export const ETH_WS = 'wss://gorli.sealcred.xyz/ws'
 export const SCLEDGER_CONTRACT_ADDRESS =
-  '0x11E44Ee2F9B5Ae5f9bD7d018A3dcdE7eDaEdD212'
+  '0xeD9bb6be9d5a487EBECb0B513460Ed5E75b87fB3'
 export const DOSU_INVITES_CONTRACT_ADDRESS =
-  '0x722B0676F457aFe13e479eB2a8A4De88BA15B2c6'
+  '0xDa3f10D9C098e55eeab0169Df0418C60ED3D8995'
 export const DOSU_INVITES_DERIVATIVE_CONTRACT_ADDRESS =
   '0x7c813AEb67B578b5a70D297BbF25958D214D0AAB'
 export const QUERY_BLOCK_LIMIT = -100000
