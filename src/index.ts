e34a9ffe4d40db1bba04af159c8eeecec18f00c4
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-13 11:30:14 -0700

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export const FORTMATIC_KEY = 'pk_test_59D9E782219D2943'
-export const ETH_NETWORK = 'rinkeby'
-export const ETH_RPC = 'https://rinkeby.sealcred.xyz/rpc'
-export const ETH_WS = 'wss://rinkeby.sealcred.xyz/ws'
+export const ETH_NETWORK = 'goerli'
+export const ETH_RPC = 'https://gorli.sealcred.xyz/rpc'
+export const ETH_WS = 'wss://gorli.sealcred.xyz/ws'
 export const SCLEDGER_CONTRACT_ADDRESS =
   '0x11E44Ee2F9B5Ae5f9bD7d018A3dcdE7eDaEdD212'
 export const DOSU_INVITES_CONTRACT_ADDRESS =
   '0x722B0676F457aFe13e479eB2a8A4De88BA15B2c6'
 export const DOSU_INVITES_DERIVATIVE_CONTRACT_ADDRESS =
   '0x7c813AEb67B578b5a70D297BbF25958D214D0AAB'
-export const QUERY_BLOCK_LIMIT = 10654475
+export const QUERY_BLOCK_LIMIT = -100000
