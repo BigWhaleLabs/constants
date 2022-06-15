3a49d876ab1cd172cafd9a86de96e879295d485d
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-14 19:04:02 -0700

    change sc ledger

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export const ETH_NETWORK = 'goerli'
 export const ETH_RPC = 'https://gorli.sealcred.xyz/rpc'
 export const ETH_WS = 'wss://gorli.sealcred.xyz/ws'
 export const SCLEDGER_CONTRACT_ADDRESS =
-  '0xeD9bb6be9d5a487EBECb0B513460Ed5E75b87fB3'
+  '0xEB0cB50a5C12B376aaf555E72FE06a6ECA154292'
 export const DOSU_INVITES_CONTRACT_ADDRESS =
   '0xDa3f10D9C098e55eeab0169Df0418C60ED3D8995'
 export const DOSU_INVITES_DERIVATIVE_CONTRACT_ADDRESS =
