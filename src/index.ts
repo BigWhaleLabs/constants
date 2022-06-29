e445be0abef88e63323b8158f442f74f5cabf46c
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-29 08:43:32 -0700

    bump dependencies and update addresses

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export const ETH_NETWORK = 'goerli'
 export const ETH_RPC = 'https://goerli.sealcred.xyz/rpc'
 export const ETH_WS = 'wss://gorli.sealcred.xyz/ws'
-export const SCLEDGER_CONTRACT_ADDRESS =
-  '0xEB0cB50a5C12B376aaf555E72FE06a6ECA154292'
-export const DOSU_INVITES_CONTRACT_ADDRESS =
-  '0x7aB9ECe4C506A87A5f6555542B9b205851aBc002'
+export const SC_ERC721_LEDGER_CONTRACT_ADDRESS =
+  '0x23D23D705F00580852075add1149BF2db059512f'
+export const SC_EMAIL_LEDGER_CONTRACT_ADDRESS =
+  '0x5AA6b79A8ea7c240c8DE59a83765AC984912A8f3'
