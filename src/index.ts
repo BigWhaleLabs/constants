448a7532a991bdc486bf0a07cd2668031458eb6d
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-08 20:32:51 -0700

    update constants

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 export const ETH_NETWORK = 'goerli'
 export const ETH_RPC = 'https://goerli.sealcred.xyz/rpc'
 export const ETH_RPC_MAINNET = 'https://eth.sealcred.xyz/rpc'
-export const ETH_WS = 'wss://gorli.sealcred.xyz/ws'
+export const EXTERNAL_SC_ERC721_LEDGER_CONTRACT_ADDRESS =
+  '0x88d5A525D5Ae030bCb66BFCec92EE7E051861732'
 export const SC_ERC721_LEDGER_CONTRACT_ADDRESS =
-  '0x23D23D705F00580852075add1149BF2db059512f'
+  '0xDbf1d0AEc19A7799181bCF582412083e102A14F8'
 export const SC_EMAIL_LEDGER_CONTRACT_ADDRESS =
-  '0x5AA6b79A8ea7c240c8DE59a83765AC984912A8f3'
+  '0x060a92F0fa5C6Ef1091cDa03ED423ECddaf5321B'
