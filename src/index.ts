091beeb30a08959c18913a68430b4de49828b1ad
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-11 07:18:27 -0700

    add reserved contract metadata

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,14 @@ export const SC_ERC721_LEDGER_CONTRACT_ADDRESS =
   '0xBaC9e2acB03bD6D37Efb2Ee24AF678F32651D3e2'
 export const SC_EMAIL_LEDGER_CONTRACT_ADDRESS =
   '0x5f4B91Cc6da3593881Ef0dEF4937550114E24CCd'
+export const reservedContractMetadata = {
+  '0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85': {
+    name: 'Ethereum Name Service',
+    symbol: 'ENS',
+  },
+} as {
+  [address: string]: {
+    name: string
+    symbol: string
+  }
+}
