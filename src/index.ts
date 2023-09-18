b4eb303838d849ed82e5d750146b18182d32a140
T_DaMER <dpancov64@gmail.com>
2023-09-18 22:22:11 +0300

    remove trailing slash

--- src/index.ts
+++ src/index.ts
@@ -80,7 +80,7 @@ export const KETL_NOTIFICATIONS_BACKEND = 'https://notifications.ketl.xyz'
 export const KETL_AI_BACKEND = 'https://ai.ketl.xyz'
 export const KETL_PFP_GENERATOR = 'https://pfp.ketl.xyz'
 export const KETL_VIEWS_BACKEND =
-  'https://bwl-ketl-views-counter.dwfjvu.easypanel.host/'
+  'https://bwl-ketl-views-counter.dwfjvu.easypanel.host'
 export const KETL_STORE_VERSION = 2.2
 
 export const METADATA_LEDGER_CONTRACT_ADDRESS =
